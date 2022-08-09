bc3b78dc3fe524dbba43fae69a56cd7bed6d0a2a
Godfrey Bongomin <bongomin@uw.edu>
2022-08-08 19:44:01 -0700

    Added logout, view profile links in the Nav directory

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,21 +3,43 @@ import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
 function Nav() {
+  const showNavigation = () => {
+    if (Auth.loggedIn()) {
+      return (
+        <div className="flex-row">
+          <ul className="flex-row">
+            <li className="navItem">
+              <Link to="/profile">View Profile</Link>
+            </li>
+            <li className="navItem">
+              <Link to="" onClick={() => Auth.logout()}>
+                logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <ul className="flex-row">
+          <li className="navItem">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="navItem">
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+      );
+    }
+  };
+
   return (
-    <div className="flex-row">
+    <header className="flex-row">
       <h1 className="hover_1">
         <Link to="/">THAT GROCERY STORE</Link>
       </h1>
-
-      <ul className="flex-row">
-        <li className="navItem">
-          <Link to="/login">Login</Link>
-        </li>
-        <li className="navItem">
-          <Link to="/signup">Signup</Link>
-        </li>
-      </ul>
-    </div>
+      <nav>{showNavigation()}</nav>
+    </header>
   );
 }
 
